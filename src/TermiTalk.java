bcdff4104d260d03b3377f6ce41c13d86b344c8b
Shanmukh Das <shanmukhxd@gmail.com>
2026-03-07 19:52:12 +0530

    Added a command line

--- src/TermiTalk.java
+++ src/TermiTalk.java
@@ -16,6 +16,12 @@ public class TermiTalk {
             server = new ServerSocket(8888);
             System.out.println("Server started. Waiting for client...");
             socket = server.accept();
+            String clientIP = socket.getInetAddress().getHostAddress();
+            System.out.println("Connected from: " + clientIP);
+            if(!clientIP.equals("192.168.29.142")) {
+                socket.close();
+                System.exit(0);
+            }
             System.out.println("Client connected!");
         } catch (IOException e) {
             System.out.println("Error: " + e.getMessage());
@@ -29,12 +35,21 @@ public class TermiTalk {
                 String msg = " ";
                 while(true){
                     msg = in.readLine();
-                    if(msg.equalsIgnoreCase("server.exit"))
-                    System.exit(0);
-                    if(msg.equalsIgnoreCase("server.open-chrome"))
-                        Runtime.getRuntime().exec("C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe");
-                    if(msg.equalsIgnoreCase("server.open-music"))
-                        Desktop.getDesktop().browse(new URI("https://music.youtube.com"));
+                    if(msg.startsWith("server.")){
+                        String cmd = msg.substring(7);
+                        if(cmd.equalsIgnoreCase("exit"))
+                            System.exit(0);
+                        if(cmd.equalsIgnoreCase("open-chrome"))
+                            Runtime.getRuntime().exec("C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe");
+                        if(cmd.equalsIgnoreCase("open-music"))
+                            Desktop.getDesktop().browse(new URI("https://music.youtube.com"));
+                    }
+                    if(msg.startsWith("server.cmd ")) {
+                        String cmd = msg.substring(11);
+                        Process proc = Runtime.getRuntime().exec("cmd /c" + cmd);
+                        BufferedReader read = new BufferedReader(new InputStreamReader(proc.getInputStream()));
+                        out.println(read.readLine());
+                    }
                     System.out.println("Client: " + msg);
                 }
             } catch(Exception e) {
