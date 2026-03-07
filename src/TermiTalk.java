5da0c92474c7aac3b17b079b3d48e6f0c82da5d2
Shanmukh Das <shanmukhxd@gmail.com>
2026-03-07 19:28:45 +0530

    Bug fixes Added exit command - server.exit Added custom commands Added multithreading for simultaneous messaging

--- src/TermiTalk.java
+++ src/TermiTalk.java
@@ -1,3 +1,4 @@
+import java.awt.*;
 import java.net.*;
 import java.io.*;
 import java.util.*;
@@ -6,7 +7,7 @@ public class TermiTalk {
     static ServerSocket server;
     static Socket socket;
     static TermiTalk talk = new  TermiTalk();
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException, URISyntaxException {
         talk.startServer();
         talk.chat();
     }
@@ -17,19 +18,38 @@ public class TermiTalk {
             socket = server.accept();
             System.out.println("Client connected!");
         } catch (IOException e) {
-            e.printStackTrace();
+            System.out.println("Error: " + e.getMessage());
         }
     }
-    void chat() throws IOException {
+    void chat() throws IOException, URISyntaxException {
         BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
         PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
-        Scanner sc = new Scanner(System.in);
-        String msg = " ";
-        while (true) {
-            msg = in.readLine();
-            System.out.println("Client: " + msg);
-            msg = sc.nextLine();
-            out.println("Server: " + msg);
-        }
+        Thread receiveThread = new Thread(() -> {
+            try{
+                String msg = " ";
+                while(true){
+                    msg = in.readLine();
+                    if(msg.equalsIgnoreCase("server.exit"))
+                    System.exit(0);
+                    if(msg.equalsIgnoreCase("server.open-chrome"))
+                        Runtime.getRuntime().exec("C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe");
+                    if(msg.equalsIgnoreCase("server.open-music"))
+                        Desktop.getDesktop().browse(new URI("https://music.youtube.com"));
+                    System.out.println("Client: " + msg);
+                }
+            } catch(Exception e) {
+                System.out.println("Error");
+            }
+        });
+        receiveThread.start();
+        Thread sendThread  = new Thread(() -> {
+            Scanner sc = new Scanner(System.in);
+            String msg = " ";
+            while(true){
+                msg = sc.nextLine();
+                out.println("Server: " + msg);
+            }
+        });
+        sendThread.start();
     }
 }
